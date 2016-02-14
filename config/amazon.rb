443e1a4b972fc5d723f552cd4d53e67abafe0316
David Ladowitz <david@ladowitz.com>
2016-02-13 21:29:17 -0800

    updating amazon ordering page

--- config/amazon.rb
+++ config/amazon.rb
@@ -2,8 +2,12 @@
 
 # Two Guys In a Garage
 # Promised: 4 markers / $6
-# Sending: Extra eraser and spray
+# Sending: Extra eraser and spray (seems like price on original went up)
 # http://www.amazon.com/Expo-Original-Chisel-6-Piece-Assorted/dp/B00006IFIV/ref=sr_1_13?s=office-products&ie=UTF8&qid=1451422150&sr=1-13&keywords=dry+erase+markers&refinements=p_85%3A2470955011%2Cp_n_location_browse-bin%3A6473192011%2Cp_n_is-min-purchase-required%3A5016683011
+# http://www.amazon.com/Expo-Black-Chisel-Markers-Marker/dp/B00LYMI07I/ref=sr_1_14?s=office-products&rps=1&ie=UTF8&qid=1452391205&sr=1-14&keywords=dry+erase+markers&refinements=p_85%3A2470955011%2Cp_n_is-min-purchase-required%3A5016683011%2Cp_n_location_browse-bin%3A6473192011
+# http://www.amazon.com/Quartet-Markers-EnduraGlide-Chisel-5001-12M/dp/B000J07IX8/ref=sr_1_15?s=office-products&rps=1&ie=UTF8&qid=1452391205&sr=1-15&keywords=dry+erase+markers&refinements=p_85%3A2470955011%2Cp_n_is-min-purchase-required%3A5016683011%2Cp_n_location_browse-bin%3A6473192011
+# http://www.amazon.com/Quartet-Markers-EnduraGlide-Assorted-5001-10M/dp/B001HA0N2M/ref=sr_1_20?s=office-products&rps=1&ie=UTF8&qid=1452391205&sr=1-20&keywords=dry+erase+markers&refinements=p_85%3A2470955011%2Cp_n_is-min-purchase-required%3A5016683011%2Cp_n_location_browse-bin%3A6473192011
+# http://www.amazon.com/Quartet-Dry-Erase-Markers-EnduraGlide-5001-31M/dp/B00SXEPAZ0/ref=sr_1_39?s=office-products&rps=1&ie=UTF8&qid=1452391421&sr=1-39&keywords=dry+erase+markers&refinements=p_85%3A2470955011%2Cp_n_is-min-purchase-required%3A5016683011%2Cp_36%3A-600
 
 # Seed Money
 # Promised: 8 markers / $7
